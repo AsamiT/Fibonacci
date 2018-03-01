965bf7f027e48f804deaae5f0876aee712138f0b
Asami Tachibana <atachibana2612@gmail.com>
2018-02-28 22:05:30 -0500

    28 Feb 2018 22:05:30

--- Fibonacci.c
+++ Fibonacci.c
@@ -60,10 +60,10 @@ HugeInteger *hugeDestroyer(HugeInteger *p) {
 int chkint(unsigned int n) {
 
     if (n == 0) {
-        return n;
+        return n; //if n is zero, then we're okay.
     }
 
-    else {
+    else { //otherwise, forget it and kick null back to the main.
         return NULL;
     }
 
@@ -74,14 +74,14 @@ HugeInteger *parseInt(unsigned int n) {;
     unsigned long int tempint = n; //assign the value of n to a temporary long int
     unsigned long int int_len = 0; //create new marker for our length
     if (!n) {
-        chkint(n);
+        chkint(n); //check n if the compiler thinks it's a null value.
     }
-    if (n == 0) {
+    if (n == 0) { //if n = 0
         a->length=1;
         a->digits=malloc((sizeof(int) * a->length+1));
         a->digits[0] = n;
     }
-    else {
+    else { //if n > 0
         while (tempint > 0) { //execute a while loop that counts up the length of our integer number
             tempint /= 10;
             int_len++;
@@ -100,31 +100,35 @@ HugeInteger *parseInt(unsigned int n) {;
 }
 
 HugeInteger *hugeAdd(HugeInteger *p, HugeInteger *q) {
+    /** This function creates a new array based off of HugeInteger,
+        along with some variables to do number management. After creating unsigned
+        long long integers with the number stored in each array (p and q), it creates
+        a sum number (int_res), which is then pushed to the new array. **/
     struct HugeInteger *array = malloc(sizeof(struct HugeInteger));
     unsigned long long int a_add = 0;
     unsigned long long int b_add = 0;
     unsigned long long int int_res = 0;
     for (int x=0; x<=p->length; x++) {
-        a_add += (p->digits[x] * intpow(10,x));
+        a_add += (p->digits[x] * intpow(10,x)); //see intpow() above for function purpose
     }
     for (int z=0; z<=q->length; z++) {
-        b_add += (q->digits[z] * intpow(10,z));
+        b_add += (q->digits[z] * intpow(10,z)); //see intpow() for more info
     }
-    int_res = a_add + b_add;
-    unsigned long long int temp_int = int_res;
+    int_res = a_add + b_add; //sum storage
+    unsigned long long int temp_int = int_res; //temporary integer storage
     int count = 0;
     while (temp_int != 0) {
         temp_int /=10;
         count++;
     }
-    array->length = count;
+    array->length = count; //push length of array to count to keep track
     array->digits= malloc(sizeof(int) * array->length+1); //dynamically create our digits array
     for (int i = 0; i < array->length; i++) {
         temp_int = int_res % 10;
         array->digits[i] = temp_int;
         int_res /= 10;
     }
-    return array;
+    return array; //give the array back, i don't want it
 }
 
 
@@ -151,18 +155,18 @@ unsigned int *toUnsignedInt(HugeInteger *p) {
 
 
 HugeInteger *fib(int n) {
-    return NULL;
+    return NULL; //WIP
 }
 
 
 double difficultyRating(void) {
-    double diff = 5.0;
+    double diff = 5.0; //pretty god damn difficult
     return diff;
 }
 
 
 double hoursSpent(void) {
-    int hrs = 40;
+    int hrs = 120; //way too many hours dude
     return hrs;
 
 }
